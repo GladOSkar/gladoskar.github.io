0aed4ea106bb095f0cb8703a27b1601cf02d4a92
GladOSkar <okkathe97th@gmail.com>
2020-12-15 22:24:44 +0100

    Add FSM thingy

---
+++ fsm/README.md
@@ -0,0 +1,4 @@
+# Finite State Machine Designer and Tester
+
+A finite state machine designer that can export diagrams to LaTeX, PNG, SVG and test string inputs on
+deterministic finite automota. Initially created by Evan Wallace (http://madebyevan.com/fsm/) and updated by David Miller to test string inputs.
---
+++ fsm/src/elements/node.js
@@ -0,0 +1,79 @@
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+function Node(x, y) {
+	this.x = x;
+	this.y = y;
+	this.mouseOffsetX = 0;
+	this.mouseOffsetY = 0;
+	this.isAcceptState = false;
+	this.text = '';
+
+	this.id = guid()
+	this.transitions = {}
+}
+
+Node.prototype.check = function(word, is_det) {
+	if (word.length < 1) {
+		return this.isAcceptState
+	}
+	else
+	{
+		if (is_det || !("\\epsilon" in this.transitions))
+		{
+			return this.transitions[word.charAt(0)].check(word.substring(1), is_det)
+		}
+		else
+		{
+			return ((this.transitions[word.charAt(0)].check(word.substring(1), is_det))||(this.transitions["\\epsilon"].check(word, is_det)))
+		}
+	}
+}
+
+Node.prototype.setMouseStart = function(x, y) {
+	this.mouseOffsetX = this.x - x;
+	this.mouseOffsetY = this.y - y;
+};
+
+Node.prototype.setAnchorPoint = function(x, y) {
+	this.x = x + this.mouseOffsetX;
+	this.y = y + this.mouseOffsetY;
+};
+
+Node.prototype.draw = function(c) {
+	// draw the circle
+	c.beginPath();
+	c.arc(this.x, this.y, nodeRadius, 0, 2 * Math.PI, false);
+	c.stroke();
+
+	// draw the text
+	drawText(c, this.text, this.x, this.y, null, selectedObject == this);
+
+	// draw a double circle for an accept state
+	if(this.isAcceptState) {
+		c.beginPath();
+		c.arc(this.x, this.y, nodeRadius - 6, 0, 2 * Math.PI, false);
+		c.stroke();
+	}
+};
+
+Node.prototype.closestPointOnCircle = function(x, y) {
+	var dx = x - this.x;
+	var dy = y - this.y;
+	var scale = Math.sqrt(dx * dx + dy * dy);
+	return {
+		'x': this.x + dx * nodeRadius / scale,
+		'y': this.y + dy * nodeRadius / scale,
+	};
+};
+
+Node.prototype.containsPoint = function(x, y) {
+	return (x - this.x)*(x - this.x) + (y - this.y)*(y - this.y) < nodeRadius*nodeRadius;
+};
